362085f183d2f066e7dc6a429041980e9c5c56f6
Alvin <alvinzcoxiv@gmail.com>
2020-08-20 23:14:21 -0400

    database created and ran, table populated with seed data

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("The Classic");
+INSERT INTO burgers (burger_name) VALUES ("The Bacon and Burbon");
+INSERT INTO burgers (burger_name) VALUES ("The Sea Cow");
